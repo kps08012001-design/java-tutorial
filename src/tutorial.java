2dcf6ac7dc37052812f81fd5231d9840f43950ac
kps08012001-design <kps08012001@gmail.com>
2026-04-29 21:10:34 +0630

    changes in git

--- src/tutorial.java
+++ src/tutorial.java
@@ -1,23 +1,36 @@
-import java.util.Scanner;
+
 
 public class tutorial{
+   private String name;
+   public int nrc;
+   int age=25;
+   protected String address;
+
+
+
+    public void DisplayInfo(){
+        System.out.println("Name is:"+ name);
+        System.out.println("Age is:"+ age);
+        System.out.println("NRC is:"+ nrc);
+        System.out.println("Address is:"+ address);
+
+
+    }
     public static void main (String[]args){
-        String choice="yes";
-        while(choice.equals("yes")){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter Your Number:");
-        String value=sc.nextLine();
-        int num=Integer.parseInt(value);
-        if(num%2==0){
-            System.out.println("Even is"+num);
-        }else{
-            System.out.println("Odd is"+num);
-        }
-        System.out.println("Do another YES/No?");
-        choice=sc.nextLine();
+        tutorial t=new tutorial();
+        t.name="Kaung Pyae Sone";
+        t.age=25;
+        t.nrc=123456;
+        t.address="Yangon";
+        t.DisplayInfo();
+
+
+     
+
+        
+
 
     
-    }
 }
 
 }
